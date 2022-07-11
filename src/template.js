97c1839a91ed879fce7f481e45103791377dff7d
mackgabe <mackenzieabe@gmail.com>
2022-07-11 10:05:02 -0400

    added in html divs and lists for build team function

--- src/template.js
+++ src/template.js
@@ -1,24 +1,50 @@
+
+//add in icons?
+
 function buildTeam(team) {
 
     function buildEngineer(engineer) {
         return `<div>
             <h1>${engineer.getName()}</h1>
-        </div>`
-    }
+        </div>
+        <div>
+        <ul>
+        <li> "ID: " + ${engineer.getId} </li>
+        <li> "Email: " + ${engineer.getEmail} </li>
+        <li> "Github: " + ${engineer.getGitHub}</li>
+        </ul>
+        </div>
+    `}
     function buildIntern(intern) {
-
-    }
+        return `<div>
+        <h1>${intern.getName()}</h1>
+        </div>
+        <div>
+        <ul>
+        <li> "ID: " + ${intern.getId} </li>
+        <li> "Email: " + ${intern.getEmail} </li>
+        <li> "School: " + ${intern.getSchool}</li>
+        </ul>
+        </div>
+        `}
     function buildManager(manager) {
         return `<div>
-            <h1>${manger.getName()}</h1>
-        </div>`
-    }
+            <h1>${manager.getName()}</h1>
+        </div>
+        <div>
+        <ul>
+        <li> "ID: " + ${manager.getId} </li>
+        <li> "Email: " + ${manager.getEmail} </li>
+        <li> "Office Number: " + ${manager.getOfficeNumber}</li>
+        </ul>
+        </div>
+        `}
 
-    const members = [] 
-    
+    const members = []
+
     members.push(team.filter(teamate => teamate.getRole() === 'Manager').map(manager => buildManager(manager)))
 
-    
+
 
     return members.join('')
 }
