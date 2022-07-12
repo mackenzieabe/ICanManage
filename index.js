2ef97ff58e68488ca9d8026e07aa42ec55ca1bd5
mackgabe <mackenzieabe@gmail.com>
2022-07-11 20:34:25 -0400

    updated add engineer prompt

--- index.js
+++ index.js
@@ -32,50 +32,76 @@ inquirer.prompt([
     const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
     teamMembers.push(manager)
     console.log(manager);
-    // mainQuestion()
-    createTeam()
+    mainQuestion()
+    // createTeam()
 })
 
 
 function mainQuestion() {
-inquirer.prompt([
-    {
-        type: 'list',
-        name: 'employees',
-        message: 'Please choose one of the following:',
-        choices: ['Engineer', 'Intern', 'Done'] 
-    }
-])
+    inquirer.prompt([
+        { 
+            type: 'list',
+            name: 'main',
+            message: 'Please choose one of the following:',
+            choices: ['Engineer', 'Intern', 'Done']
+        }
+    ]).then(answer => {
+        switch (answer.main) {
+            case 'Engineer':
+                addEngineer()
+                break;
+            case 'Intern':
+                addIntern()
+                break;
+            default:
+                createTeam()
+        }
+    })
 }
 
-function addEngineer() {//is this taking care of redirecting manager to these prompts?
-inquirer.prompt([
-    {
-        type:'input',
-        name: 'username',
-        message: 'What is the github username of the engineer?'
-    }
-    //Do i need to do the .then answers after each prompt or just at the end?
-]).then(answers => {
-    const engineer= new Engineer(answers.github)
-    teamMembers.push(engineer)
-    createTeam()
-})
+function addEngineer() {
+    inquirer.prompt([
+        {  
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the ID of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is the github username of the engineer?'
+        }
+        
+    ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email,answers.github)
+        teamMembers.push(engineer)
+        mainQuestion()
+    })
 }
 
 
 function addIntern() {
-inquirer.prompt ([
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What school did the intern attend?'
-    }
-]).then(answers=> {
-    const intern = new Intern (answers.school)
-    teamMembers.push(intern)
-    createTeam()
-})
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school did the intern attend?'
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.school)
+        teamMembers.push(intern)
+        mainQuestion()
+    })
 }
 //function addDone() ???
 function createTeam() {
@@ -89,3 +115,17 @@ function createTeam() {
         console.log("Team has been built!")
     })
 }
+
+//why did it only track the manager prompt?
+// ? What is the name of the manager? Mackenzie
+// ? What is the id of the manager? 5
+// ? What is the email of the manager? mackenzieabe@gmail.com
+// ? What is the office number of the manager? 240
+// Manager {
+//   name: 'Mackenzie ',
+//   id: '5',
+//   email: 'mackenzieabe@gmail.com',
+//   officeNumber: '240'
+// }
+// file created
+// Team has been built!
--- src/template.js
+++ src/template.js
@@ -2,18 +2,31 @@
 //add in icons?
 //how do I add an href for the email? 
 function buildTeam(team) {
-
+    
+    function buildManager(manager) {
+        return `<div>
+        <h1>${manager.getName()}</h1>
+        </div>
+        <div>
+        <ul>
+        <li> "ID: " + ${manager.getId()} </li>
+        <li> 
+        <a href="mailto:${manager.getEmail()}> "Email: " + ${manager.getEmail()}</a></li>
+        <li> "Office Number: " + ${manager.getOfficeNumber()}</li>
+        </ul>
+        </div>
+        `}
     function buildEngineer(engineer) {
         return `<div>
             <h1>${engineer.getName()}</h1>
         </div>
+        <div><img src='' alt=''> Engineer</div>
         <div>
         <ul>
-        <li> "ID: " + ${engineer.getId} </li>
-        <li> "Email: " + ${engineer.getEmail} 
-        <a href="mailto:${engineer.getEmail}></li>
-        <li> "Github: " + ${engineer.getGitHub}
-        <a href= "https://github.com/${engineer.getGitHub}></li>
+        <li> "ID: " + ${engineer.getId()} </li>
+        <li>  
+        <a href="mailto:${engineer.getEmail()}>"Email: " + ${engineer.getEmail()}</a></li>
+        <li><a href= "https://github.com/${engineer.getGitHub()}>"Github: " + ${engineer.getGitHub()}</a></li>
         </ul>
         </div>
     `}
@@ -23,32 +36,22 @@ function buildTeam(team) {
         </div>
         <div>
         <ul>
-        <li> "ID: " + ${intern.getId} </li>
-        <li> "Email: " + ${intern.getEmail}
-        <a href="mailto:${intern.getEmail}> </li>
-        <li> "School: " + ${intern.getSchool}</li>
+        <li> "ID: " + ${intern.getId()} </li>
+        <li> 
+        <a href="mailto:${intern.getEmail()}>"Email: " + ${intern.getEmail()}</a></li>
+        <li> "School: " + ${intern.getSchool()}</li>
         </ul>
         </div>
         `}
-    function buildManager(manager) {
-        return `<div>
-            <h1>${manager.getName()}</h1>
-        </div>
-        <div>
-        <ul>
-        <li> "ID: " + ${manager.getId} </li>
-        <li> "Email: " + ${manager.getEmail} 
-        <a href="mailto:${manager.getEmail}></li>
-        <li> "Office Number: " + ${manager.getOfficeNumber}</li>
-        </ul>
-        </div>
-        `}
+
 
     const members = []
 
     members.push(team.filter(teamate => teamate.getRole() === 'Manager').map(manager => buildManager(manager)))
 
+    members.push(team.filter(teamate => teamate.getRole() === 'Engineer').map(engineer => buildEngineer(engineer)))
 
+    members.push(team.filter(teamate => teamate.getRole() === 'Intern').map(intern => buildIntern(intern)))
 
     return members.join('')
 }
@@ -60,8 +63,8 @@ module.exports = (team) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>Document</title>
+        <link rel='stylesheet' href ='style.css'>
+        <title>Team Members</title>
     </head>
     <body>
         ${buildTeam(team)}
